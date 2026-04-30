7d49cb47e153ed2ff96a1b5c9fafa6d558ef8de9
Abhirup Bhowmick <abhirupbhowmick111777@gmail.com>
2026-05-01 03:56:12 +0530

    chore: update to correct OAuth Client ID for production

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
-const GOOGLE_CLIENT_ID = "509480956446-26ag8h7hrpmdss2t1dteb655olsrflbo.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "509480956446248lc30aonsbjhhkj485lvtbnb2rkp7l.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
